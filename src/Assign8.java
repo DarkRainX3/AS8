804de135bdca2f78fcb4a29d25beed9f90f60a02
Waley <44916399+DarkRainX3@users.noreply.github.com>
2018-12-04 00:19:59 -0700

    First

---
+++ src/Assign8.java
@@ -0,0 +1,10 @@
+
+public class Assign8 {
+	public static void main(String [] args) {
+		if (args.length!=3) {
+			System.out.println("Invalid Number of Arguments");
+			System.exit(0);
+		}
+	}
+
+}
